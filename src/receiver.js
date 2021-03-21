c200f14e16e3704f2bd68ae071999d2f64e835f9
v01d13 <ashesh.rai666@gmail.com>
2021-03-21 23:00:56 +0545

    Receiver now receives the sent file properly.

--- src/receiver.js
+++ src/receiver.js
@@ -1,28 +1,36 @@
 // Importing required modules
 const http = require('http').createServer();
 const io = require('socket.io')(http);
-// Check whether stream modules are same or not in sender and receiver
-const ss = require('@sap_oss/node-socketio-stream');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+var filename = null;
+var buff = [];
 // Connect event
 io.on("connect", (socket) => {
-  console.log('Connected');
-  // Casting socketio ko socketio stream and receiving the stream and data about the file from the emit event
-    ss(socket).on('file', (stream, data, err) => {
-      if(err) return console.error(err);
-      console.log('Receiving file.');
-      var filename = path.basename(data.name);
-      var writeStream = fs.createWriteStream(filename);
-      stream.pipe(writeStream);
-      console.log('File received.');
-      writeStream.on('error', (err) => {
-        return console.error (err);
+    console.log('Connected');
+    socket.on('hello', (data) => {
+      console.log(data);
+    });
+    // Casting socketio ko socketio stream and receiving the stream and data about the file from the emit event
+    socket.on('file', (chunk, fileName) => {
+      console.log(`Receiving file: ${filename}`);
+      filename = path.basename(fileName);
+      buff.push(chunk);
+    });
+    socket.on('ending', (data) => {
+      console.log(`${filename} writestream closed.`);
+      var finalBuff = Buffer.concat(buff);
+      fs.writeFile(filename, finalBuff, (err) => {
+        if (err) return console.error(err);
+          process.exit(1);
       });
     });
-  console.log('Outside on');
+});
+// Disconnect event
+io.on('disconnect', () => {
+  console.log('Disconnected')
 });
 // Server listening on
-http.listen(8888, '192.168.1.92', () => {
-  console.log(`Listening : ${http.address().address}:${http.address().port}`)
+http.listen(10000, '192.168.1.5', () => {
+  console.log(`Listening: ${http.address().address}`)
 });
--- src/sender.js
+++ src/sender.js
@@ -1,43 +1,35 @@
 // Required modules
 const io = require('socket.io-client');
-// Check whether stream modules are same or not in sender and receiver
-const ss= require('@sap_oss/node-socketio-stream');
 const fs = require('fs');
 // Connect to the server using connect function
-const socket = io.connect('http://192.168.1.101:8888');
+const socket = io.connect('http://192.168.1.134:10000');
 
-// Create a stream for the socket io
-const stream = ss.createStream();
 // Insert filename here in editor here for now
-var filename ="/home/flutterdev/Programming/FlutterApp/file_transfer/src/flutterautomate.mp4";
-// Cast socketio connection to socket.io-stream and emit the event
-ss(socket).emit('file', stream, {name: filename});
-// Create readstream from the fs module and pipe to the socket stream
-
-// readStream.pipe(stream);
-// Error handling for socket and readStream
-
+var filename ="ragnarok.mp4";
+// 
 
 socket.on('connect',(data)=>{
-  socket.emit("msg","Hello from pc")
   // socket.emit('file',readStream)
-  console.log("The connection is successful")
-var readStream = fs.createReadStream(filename)
-// readStream.setEncoding('utf8');
-readStream.on('data',(chunk)=>{
-  socket.emit('file',chunk)
-console.log("THe chunk is ",chunk)
-})
-readStream.on('close',()=>{
-  console.log("NOw closing the stream")
-  // socket.emit('close',"close");
-})
-readStream.on('error', (err) => {
-  return console.error(err);
-})
+  console.log('Connected.');
+  var readStream = fs.createReadStream(filename);
+  // readStream.setEncoding('utf8');
+  readStream.on('data', (chunk) => {
+    console.log(`Sending: ${filename}`);
+    //Changing chunk file format to string for easier writing
+    var chunkString = chunk.toString();
+    //Emitting the string to the receiver
+    socket.emit('file', chunkString, filename);
+    })
+    readStream.on('close',()=>{
+      // Emitting the end of stream to the receiver
+      console.log('Closing the stream');
+      socket.emit('ending', 'close');
+    })
+    readStream.on('error', (err) => {
+      return console.error(err);
+    });
 })
 
 socket.on('error', (err) => {
   return console.error(err);
-  process.exit(1);
 })
